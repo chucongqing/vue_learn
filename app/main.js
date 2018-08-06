2d9399296412e4aa0da26c4deb639b0e5718c322
CCQ RJ_PC <chucongqing@qq.com>
2018-08-06 15:05:25 +0800

    router and statement

--- app/main.js
+++ app/main.js
@@ -1,15 +1,32 @@
 import Vue from './vue';
+ import VueRouter from './vue-router';
+Vue.use(VueRouter);
 
-import greeter from './Greeter'
-document.querySelector('#root').appendChild(greeter());
+// import greeter from './Greeter';
+// document.querySelector('#root').appendChild(greeter());
 
-import say from './util'
-say();
+// import say from './util';
+// say();
+
+const User = {
+    template:'<div>User {{$route.params.id}}</div>'
+};
+
+const Foo = {template:'<div>foo</div>'};
+const Bar = { template:'<div>bar</div>'};
+
+const routes = [
+    { path:'/foo', component:Foo},
+    { path:'/bar', component:Bar},
+    { path:'/user/:id', component: User}
+];
+
+const router = new VueRouter({
+    routes // 缩写 相当于 routes:routes
+});
+
 
 
 var app = new Vue({
-    el:"#app",
-    data:{
-        message:'Hello Vue!'
-    }
-})
+    router
+}).$mount('#app');
